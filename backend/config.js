01ec72c22603d386dae30b96f48033d1b3f17411
Tony <tonyrodriguez26@outlook.com>
2025-11-11 14:37:49 -0500

    secret key

--- backend/config.js
+++ backend/config.js
@@ -1,13 +1,11 @@
 require("dotenv").config();
 
-const DB_URI = process.env.DATABASE_URL || (
-    process.env.NODE_ENV === "test"
-        ? "postgresql:///DBNAME_test"
-        : "postgresql:///daily_discipline"
-);
+const DB_URI =
+    process.env.DATABASE_URL ||
+    (process.env.NODE_ENV === "test"
+        ? "postgresql:///daily_discipline_test"
+        : "postgresql:///daily_discipline");
 
+const SECRET_KEY = process.env.SECRET_KEY || "fallback-secret-key";
 
-const SECRET_KEY = 'verysecretivekey';
-
-
-module.exports = { DB_URI, SECRET_KEY }
+module.exports = { DB_URI, SECRET_KEY };
